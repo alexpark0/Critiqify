84134a2e3f48a510152ed887939fa749d867ec4d
alexpark0 <park.ale@northeastern.edu>
2024-03-19 18:37:52 -0400

    added top bar functionality

--- src/App.jsx
+++ src/App.jsx
@@ -17,15 +17,22 @@ const App = () => {
   const change = (event) => {
     setVal(event.target.value);
   };
+
+  const [toggle, setToggle] = useState(1)
+
+  function updateToggle(id) {
+    setToggle(id)
+  }
+
   return (
     <>
       <div className="alex-park-homescreen">
-        <button className="button-left">Home</button>
+        <button className="button-left" onClick={()=>updateToggle(1)}>Home</button>
         <h1>ALEX PARK APP</h1>
-        <button className="button-middle">About</button>
-        <button className="button-right">Contact</button>
+        <button className="button-middle" onClick={()=>updateToggle(2)}>About</button>
+        <button className="button-right" onClick={()=>updateToggle(3)}>Credits</button>
       </div>
-      <div>
+      <div className={toggle === 1 ? "show-content" : "content"}>
         <h2>Record Yourself Doing a Presentation!</h2>
         <div className="button-flex">
           <button onClick={toggleRecordOption("video")}>Record Video</button>
@@ -43,6 +50,25 @@ const App = () => {
             <input type="submit" value="Enter" />
           </form>
         </div>
+      </div>
+      <div className={toggle === 2 ? "show-content" : "content"}>
+        <h1>
+          App used to record and self-critique video presentations.
+        </h1>
+      </div>
+      <div className={toggle === 3 ? "show-content" : "content"}>
+        <h1>
+          Alex Park :   park.ale@northeastern.edu
+        </h1>
+        <h1>
+          Aiden Rim :   rim.a@northeastern.edu
+        </h1>
+        <h1>
+          Bryan Li  :   li.brya@northeastern.edu
+        </h1>
+        <h1>
+          Colin Chu :   chu.col@northeastern.edu
+        </h1>
       </div>
     </>
   );
