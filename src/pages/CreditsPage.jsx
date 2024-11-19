2cbce0bddc28fc28f1c1498cabaa8983f30e2f49
alexpark0 <park.ale@northeastern.edu>
2024-11-19 18:22:22 -0500

    horizontal credits

--- src/pages/CreditsPage.jsx
+++ src/pages/CreditsPage.jsx
@@ -1,23 +1,37 @@
+import "./CreditsPage.css";
+
 const CreditsPage = () => {
   return (
-    <>
-      <h2>Alex Park : park.ale@northeastern.edu</h2>
-      <a href="https://www.linkedin.com/in/alex-park-/">
-        <img src="alex.jpeg" alt="" width="128" height="160" />
-      </a>
-      <h2>Aiden Rim : rim.a@northeastern.edu</h2>
-      <a href="https://www.youtube.com/watch?v=DINnS09LZww">
-        <img src="aiden.jpg" alt="arim" width="128" height="160"></img>
-      </a>
-      <h2>Bryan Li : li.brya@northeastern.edu</h2>
-      <a href="https://www.linkedin.com/in/bryanli27/">
-        <img src="bryan.jpeg" alt="bryali" width="128" height="160"></img>
-      </a>
-      <h2>Colin Chu : chu.col@northeastern.edu</h2>
-      <a href="https://www.crunchyroll.com/series/GY8VEQ95Y/darling-in-the-franxx">
-        <img src="colin.jpg" alt="colchu" width="128" height="160"></img>
-      </a>
-    </>
+    <div className="credits">
+      <div>
+        <a href="https://www.linkedin.com/in/alex-park-/">
+          <img src="alex.jpeg" alt="" width="240" height="300" />
+        </a>
+        <div>Alex Park</div>
+        <div>park.ale@northeastern.edu</div>
+      </div>
+      <div>
+        <a href="https://www.linkedin.com/in/aidenrim/">
+          <img src="aiden.jpg" alt="arim" width="240" height="300" />
+        </a>
+        <div>Aiden Rim</div>
+        <div>rim.a@northeastern.edu</div>
+      </div>
+      <div>
+        <a href="https://www.linkedin.com/in/bryanli27/">
+          <img src="bryan.jpeg" alt="bryali" width="240" height="300" />
+        </a>
+        <div>Bryan Li</div>
+        <div>li.brya@northeastern.edu</div>
+      </div>
+      <div>
+        <a href="https://www.linkedin.com/in/colinchu4/">
+          <img src="colin.jpg" alt="colchu" width="240" height="300" />
+        </a>
+        <div>Colin Chu</div>
+        <div>chu.col@northeastern.edu</div>
+      </div>
+    </div>
   );
 };
 
