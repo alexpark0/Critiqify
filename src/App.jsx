de7025d169c28040cdc53ee88450b4adaf02234f
alexpark0 <park.ale@northeastern.edu>
2025-01-31 17:02:06 -0500

    slight refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import RecordPage from "./pages/RecordPage";
 import CreditsPage from "./pages/CreditsPage";
 import Navbar from "./Navbar";
-import Logs from "../src/Videos";
+import Logs from "./pages/Videos";
 
 const App = () => {
   return (
     <Router>
       <Navbar></Navbar>
-      {/* Routes for Different Pages */}
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/record" element={<RecordPage />} />
